7ddc44cf314ac7534548305bdbe34bcacac73f00
PAlagusurya <alagusuryap@gmail.com>
2025-06-25 21:52:06 +0530

    Nuances of JS

--- OB1/prototype.js
+++ OB1/prototype.js
@@ -62,7 +62,7 @@ class User {
     this.name = name;
     this.data = parsedData;
 
-    return parsedData;
+    // return parsedData;
   }
 
   getData() {
@@ -75,8 +75,28 @@ const suryaa = new User("surya", {
   shouldConnect: true,
 });
 
-console.log("SUEYA:", suryaa);
-
 console.log(suryaa.getData());
 
-//TypeError: suryaa.getData is not a function
+//TypeError: suryaa.getData is not a function with return statement
+
+//OUTPUT 4:
+
+function People(name) {
+  this.name = name;
+}
+
+People.prototype.sayName = function () {
+  console.log(this.name);
+};
+
+var suryaaaa = new People("surya");
+
+suryaaaa.sayName();
+People.prototype.sayName();
+Object.getPrototypeOf(suryaaaa).sayName();
+suryaaaa.__proto__.sayName();
+
+/**
+ * 🔹 Calling a method from the prototype object directly (People.prototype.sayName()) means this refers to the prototype itself, which doesn’t have name → so undefined.
+   🔹 Calling the method on the instance (instance.sayName()) sets this to the instance → works as expected.
+ *  */
